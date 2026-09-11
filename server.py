cd105eee6b5774b906abb325ce66a98fc042657a
Devyani <devyanipaunikar2@gmail.com>
2026-09-11 20:52:15 +0530

    Initial commit: Real-Time Collaborative Markdown Editor

---
+++ server.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from typing import List
+
+app = FastAPI()
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+        await self.broadcast_user_count()
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast_user_count(self):
+        # Send updated user count to everyone connected
+        count_message = f"COUNT:{len(self.active_connections)}"
+        for connection in self.active_connections:
+            await connection.send_text(count_message)
+
+    async def broadcast(self, message: str, sender: WebSocket):
+        for connection in self.active_connections:
+            if connection != sender:
+                await connection.send_text(message)
+
+manager = ConnectionManager()
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(data, sender=websocket)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast_user_count()
